af233482e1dc69fdd21b0d57e50d240c3b46d636
u-Hoshi <yuta000707@yahoo.co.jp>
2023-06-28 23:34:57 +0900

    7.7

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>7</main>;
+  return <main className={styles.main}>77</main>;
 }
